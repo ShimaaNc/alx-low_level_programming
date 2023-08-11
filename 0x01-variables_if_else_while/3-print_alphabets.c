885d55906ba3fca577a2a98864bb9a1545ce50db
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-08-11 13:58:10 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program that tells if a number's
+ * units digit is greater than of less than five
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
