a220a27373b4b1ee68a6aa691e69a37196566c99
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-08-15 15:46:33 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets
+ *
+ * Description: A program that prints alphabets
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+void print_alphabet(void)
+{
+int a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
