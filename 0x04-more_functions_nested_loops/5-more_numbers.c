df368bd4fd7bb7662cc923b3232d8e19facba0d2
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-08-20 01:32:20 -0700

    m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,19 @@
 */
 void more_numbers(void)
 {
-int num, row, count;
+int n1, n2;
 
-for (row = 1; row <= 10; row++)
+for (n1 = 0; n1 < 10; n1++)
 {
-	for (count = 0; count <= 14; count++)
-	{
-		num = count;
-		if (count > 9)
-		{
-			_putchar((1 + 48);
-			num = count % 10;
-		}
-		_putchar(num + 48);
-	}
-	_putchar("\n");
+for (n2 = 0; n2 <= 14; n2++)
+
+{
+if (n2 > 9)
+{
+_putchar((n2 / 10) + '0');
+}
+_putchar((n2 % 10) + '0');
+}
+_putchar(10);
 }
 }
