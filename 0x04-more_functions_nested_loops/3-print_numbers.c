dabf0af0ff9d199281a2603720944993825585da
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-08-20 01:15:23 -0700

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers - function that print 0-9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int num = 48;
+for (num = 48; num < 58; num++)
+{
+_putchar(number);
+}
+_putchar(10);
+}
