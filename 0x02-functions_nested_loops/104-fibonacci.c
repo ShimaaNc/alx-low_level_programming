a7342656384c38a33da42c27773da6403ec4a151
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-08-20 10:37:23 -0700

    m

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,45 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * _isalpha - prints 1 if character is lower case else 0
- * Description - tells if a character is lower case or not
- * @c: the character number according to ASCII
- * Return: Always 0 (Success)
- *
+*main - prints out first 98
+*fibonacci suit numbers
+*Return: return 0
 */
-int _isalpha(int c)
+int main(void)
 {
-if (c >= 97 && c <= 122)
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+unsigned long h1, h2;
+
+for (inc = 0; inc < 92; inc++)
 {
-return (1);
+n3 = n1 + n2;
+printf("%lu, ", n3);
+n1 = n2;
+n2 = n3;
 }
-else if (c >= 65 && c <= 90)
+n1_h1 = n1 / 10000000000;
+n2_h1 = n2 / 10000000000;
+n1_h2 = n1 % 10000000000;
+n2_h2 = n2 % 10000000000;
+for (inc = 93; inc < 99; inc++)
 {
-return (1);
+h1 = n1_h1 + n2_h1;
+h2 = n1_h2 + n2_h2;
+if ((n1_h2 + n2_h2) > 9999999999)
+{
+h1 += 1;
+h2 %= 10000000000;
 }
-else
-{
+printf("%lu%lu", h1, h2);
+if (inc != 98)
+printf(", ");
+
+n1_h1 = n2_h1;
+n1_h2 = n2_h2;
+n2_h1 = h1;
+n2_h2 = h2;
+}
+printf("\n");
 return (0);
-}
 }
