d49a7649f0ae56f56a7b95c24a85f82e42b1de0b
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-08-28 17:54:27 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr -  pointer to the first occurrence of the character
+ * @s: string
+ * @c: character is not found
+ * Return: 's'
+ */
+
+char *_strchr(char *s, char c);
+{
+int m = 0;
+
+for (; s[m] >= '\0'; m++)
+{
+if (s[m] == c)
+{
+return (s + m);
+}
+}
+return ('\0');
+}
