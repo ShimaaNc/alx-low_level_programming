1aadc0607c5f2b451a176b4fc1ddfb674df7354a
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-08-29 07:19:58 -0700

    shimaa

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,6 @@
 *main - header file for protoypes
 */
 
-#include < stdio.h >
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
