7d34b06643d1ff9a83150714f1936f445fd1ece4
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-09-05 16:13:05 -0700

    commit

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-int _putchar(char c);
+#include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
