2f62ea82127ae8b703494cbf65dfb0438e1244c6
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-09-05 16:23:22 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,23 +3,26 @@
 /**
 * create_array - creates an array of chars, and
 * initializes it with a specific char.
-*@size: Size of the array
+*@size: Size of array
 *@c: Character to insert
 *Return: NULL if size is zero or if it fails,
-*pointer to array initialize or NULL.
+*pointer to array if everything is normal.
 */
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
-	char *n = (char *) malloc(size * sizeof(char));
+char *a;
+unsigned int i;
 
-	if (n == NULL)
-		return (NULL);
+if (size == 0)
+return (NULL);
+a = malloc(sizeof(char) * size);
 
-	for (unsigned int i = 0; i < size; i++)
-		n[i] = c;
+if (a == NULL)
+return (NULL);
 
-	return (n);
+for (i = 0; i < size; i++)
+a[i] = c;
+
+return (array);
 }
