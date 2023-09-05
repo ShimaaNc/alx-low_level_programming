3bb4573ea70613e196150fa905849f76d0395031
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-09-05 16:06:33 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 		return (NULL);
-	char *n = (char *) malloc(size * size(char));
+	char *n = (char *) malloc(size * sizeof(char));
 
 	if (n == NULL)
 		return (NULL);
