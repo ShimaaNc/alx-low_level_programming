adca0a52673b8fd62fe6a4bc0f6243146e1f417d
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-09-05 16:36:17 -0700

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* _strdup - returns a pointer to a newly allocated space in memory
+*which contains a copy of the string given as a parameter.
+*@str:String
+*
+*Return: NULL or 0
+*/
+
+char *_strdup(char *str)
+{
+char *cp;
+int i, l;
+
+if (str == NULL)
+return (NULL);
+
+for (; str[i] != '\0'; i++)
+l++;
+cp = malloc(sizeof(char) * (l + 1));
+
+if (cp == NULL)
+return (NULL);
+
+for (i = 0; str[i]; i++)
+{
+cp[i] = str[i];
+}
+
+cp[l] = '\0';
+
+return (cp);
+
+}
