0c8926bfa37a8c873c726930193a57dc4e91238f
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-09-08 12:57:55 -0700

    commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,32 +8,36 @@
 *
 * Return:NULL if new_size =0 and  pointer is not NULL
 */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *p;
 unsigned int i;
-if (new_size == old_size)
-return (ptr);
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
 
 if (ptr == NULL)
 {
 p = malloc(new_size);
-if (p == NULL)
-return (NULL);
 return (p);
 }
-if (new_size > old_size)
+else if (new_size == old_size)
+return (ptr);
+
+else if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+else
 {
 p = malloc(new_size);
-if (p == NULL)
-return (NULL);
-for (i = 0; i < old_size && i < new_size; i++)
+if (p != NULL)
+{
+for (i = 0; i < min(old_size, new_size); i++)
 *((char *)p + i) = *((char *) ptr + i);
 free(ptr);
-}
 return (p);
+}
+else
+return (NULL);
+}
+}
