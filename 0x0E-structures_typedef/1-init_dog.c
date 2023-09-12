5b75f85fb3dd9bb9807a77246bd1e9711eda601d
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-09-11 18:34:45 -0700

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes dog
+ * @d: pointer to struct dog to initialize
+ * @name:dog is name
+ * @age:dog is age
+ * @owner: owner to initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
