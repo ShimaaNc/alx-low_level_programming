58f76fb8e5b1833cc133b6ce8f25ae470b8184e7
ShimaaNc <shimaaeltelwany842@gmail.com>
2023-09-13 18:11:03 -0700

    commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add - sum of two integers
+* @a: first int
+* @b: second int
+*
+* Return: sum tow num
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - difference two integers
+* @a: first int
+* @b: second int
+*
+* Return: difference tow num
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - product of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: product tow num
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - division two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: divisio tow num
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - remainder of the division of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: remainder tow num
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
